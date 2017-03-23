e218306dc06eece3dc75ee92222cc956aeee0e64
Alexander Corn <mckay@doctormckay.com>
2017-03-23 17:11:15 -0400

    This needs to be parsed apparently

--- tesladata.js
+++ tesladata.js
@@ -59,16 +59,18 @@ function auth() {
 			throw err;
 		}
 		
-		if (!res.body.access_token || !res.body.refresh_token || !res.body.expires_in) {
+		var body = JSON.parse(res.body);
+		
+		if (!body || !body.access_token || !body.refresh_token || !body.expires_in) {
 			throw new Error("Got malformed response");
 		}
 		
-		log("Got new refresh token " + res.body.refresh_token.substring(0, 6) + "...");
-		g_BearerToken = res.body.access_token;
-		Config.tesla.encryptedToken = McCrypto.encrypt(McCrypto.Cipher.AES256CTRWithHMAC, process.env.ENCRYPTION_KEY, res.body.refresh_token).toString('base64');
+		log("Got new refresh token " + body.refresh_token.substring(0, 6) + "...");
+		g_BearerToken = body.access_token;
+		Config.tesla.encryptedToken = McCrypto.encrypt(McCrypto.Cipher.AES256CTRWithHMAC, process.env.ENCRYPTION_KEY, body.refresh_token).toString('base64');
 		FS.writeFileSync(__dirname + '/config.json', JSON.stringify(Config, undefined, "\t"));
 		
-		g_BearerTokenExpiresTime = Date.now() + (1000 * (res.body.expires_in - (60 * 60)));
+		g_BearerTokenExpiresTime = Date.now() + (1000 * (body.expires_in - (60 * 60)));
 		getData();
 	});
 }

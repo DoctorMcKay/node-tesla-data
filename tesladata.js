c82dbf954f0702990342adad6eb4193ada4bfa26
Alex Corn <DoctorMcKay@users.noreply.github.com>
2019-06-28 13:47:11 -0400

    Make table name configurable

--- tesladata.js
+++ tesladata.js
@@ -248,11 +248,12 @@ function getData() {
 			"vehicle_config": JSON.stringify(config)
 		};
 
-		g_DB.query("INSERT INTO `tesla_data` SET ?", [cols], (err) => {
+		let tableName = Config.mysql.table || 'tesla_data';
+		g_DB.query("INSERT INTO `" + tableName + "` SET ?", [cols], (err) => {
 			if (err) {
 				if (err.message.match(/Unknown column 'vehicle_config'/)) {
 					// We use varchar here because it's less likely to fail and this is going to be an unattended alter
-					g_DB.query("ALTER TABLE `tesla_data` ADD COLUMN `vehicle_config` VARCHAR(5000) NOT NULL DEFAULT '{}' AFTER `vehicle_state`", (err) => {
+					g_DB.query("ALTER TABLE `" + tableName + "` ADD COLUMN `vehicle_config` VARCHAR(5000) NOT NULL DEFAULT '{}' AFTER `vehicle_state`", (err) => {
 						if (err) {
 							throw err;
 						}
